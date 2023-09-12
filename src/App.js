4c0c68d85ed28eeb55f61f688bcfafac3fe140b9
Mehakpreet Singh <mehakbrar811@gmail.com>
2023-09-13 00:08:22 +0530

    Second Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Page from './Components/Block1/Page';
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Home/>
+      <Page/>
     </div>
   );
 }
