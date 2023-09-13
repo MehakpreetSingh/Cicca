063027ff94d8f4fdc4093489fbbf1eeec39363c8
Mehakpreet Singh <mehakbrar811@gmail.com>
2023-09-13 23:08:34 +0530

    Fourth and Fifth Block Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Page from './Components/Block1/Page';
 import Details from './Components/Block2/Details';
 import Platform from './Components/Block3';
+import Platform_next from './Components/Block4';
+import Contributors from './Components/Block5';
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
 
@@ -14,6 +16,8 @@ function App() {
       <Page/>
       <Details/>
       <Platform />
+      <Platform_next/>
+      <Contributors/>
     </div>
   );
 }
